cf72066526f4e758e185ab9e1cdaffe028079e1a
Leandro1Ezequiel1 <81895143+Leandro1Ezequiel1@users.noreply.github.com>
2023-08-16 21:12:03 -0300

    renderizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar//NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import CartWidget from './components/CartWidget/CartWaidget'; 
 function App() {
   return (
     <div className="App">
